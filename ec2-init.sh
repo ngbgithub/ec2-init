a23c1425df47d95d71d04edf782311b320a2bbcf
ngbgithub <ngbgithub>
2024-03-01 21:26:33 +0000

    Added content to .bashrc.

--- ec2-init.sh
+++ ec2-init.sh
@@ -4,7 +4,7 @@
 
 # Define functions and contants:
 usage() { echo "usage: $0 [-h|--help] [-f|--force]"; }
-BASHRC_FILE="$PWD"/.bashrc
+BASH_CONFIG_FILE="$PWD"/.bashrc
 
 
 # Deal with the command line:
@@ -38,17 +38,39 @@ done
 # Generate a .bashrc:
 #####################
 
-read -r -d '' BASHRC <<EOF
+read -r -d '' BASH_CONFIG <<EOF
 #!/bin/bash
+
+#alias ls="ls --color=auto -F"
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+export EDITOR="emacs -nw"
+export PATH=$HOME/.local/bin:$PATH
+
+#PROMPT_COLOR="0;30m" # black
+#PROMPT_COLOR="0;31m" # red
+#PROMPT_COLOR="0;32m" # green
+#PROMPT_COLOR="1;34m" # blue
+#PROMPT_COLOR="0;35m" # purple
+#PROMPT_COLOR="0;36m" # light blue
+PROMPT_COLOR="0;37m" # gray
+
+export PS1="\[\033[${PROMPT_COLOR}\]\u@\h:\W\$ \[\033[00m\]"
+#export PS1="\[\033[${PROMPT_COLOR}\]\u:\W\$ \[\033[00m\]"
+#export PS1="\u@\h:\W\$ \[\033[00m\]"
+
 
 EOF
 
 # Bail out if a .bashrc already exists.  (I'm not including an option
-#   to force deletion; having a script delete ~/.bashrc creeps me
-#   out.)
-[ ! -f $BASHRC_FILE ] || { echo "error: $BASHRC_FILE already exists"; exit -3; }
+#   to force deletion, because having a script delete ~/.bashrc creeps
+#   me out.)
+[ ! -f $BASH_CONFIG_FILE ] || \
+    { echo "error: $BASH_CONFIG_FILE already exists"; exit -3; }
 
-echo "$BASHRC" > $BASHRC_FILE
+echo "$BASH_CONFIG" > $BASH_CONFIG_FILE
 
 
 
